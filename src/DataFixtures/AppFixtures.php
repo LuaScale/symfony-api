8864d1210be755c1f70a800cdb06ec9756d4d4b6
Jules Van Den Eede <luascale@gmail.com>
2026-01-08 15:28:50 +0100

    feat: add AppFixtures for initial data population and migration to drop product table

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Item;
+use App\Entity\Shop;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+private UserPasswordHasherInterface $hasher;
+
+public function __construct(UserPasswordHasherInterface $hasher)
+{
+$this->hasher = $hasher;
+}
+
+public function load(ObjectManager $manager): void
+{
+$category = new Category();
+$category->setName('Figurines Vintage');
+$category->setSlug('figurines-vintage');
+$manager->persist($category);
+
+$user = new User();
+$user->setEmail('vendeur@collector.shop');
+$user->setPseudo('RetroHunter');
+$user->setRoles(['ROLE_USER']);
+$user->setIsVerified(true);
+$password = $this->hasher->hashPassword($user, 'password123');
+$user->setPassword($password);
+$manager->persist($user);
+
+$shop = new Shop();
+$shop->setName('La Caverne aux Merveilles');
+$shop->setDescription('Spécialiste des jouets des années 80.');
+$shop->setOwner($user);
+$manager->persist($shop);
+
+$item = new Item();
+$item->setName('Goldorak Jumbo Shogun');
+$item->setDescription('Figurine géante en plastique, très bon état, boite d\'origine.');
+$item->setPrice(25000);
+$item->setStatus('VALIDATED');
+$item->setShop($shop);
+$item->setCategory($category);
+$item->setCreatedAt(new \DateTimeImmutable());
+$manager->persist($item);
+
+$manager->flush();
+}
+}
